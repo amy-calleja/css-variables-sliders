9778444a2258f50b5421a56a901c9d368ca8d88e
AmyCalleja <amy.calleja@live.com>
2022-06-20 11:53:59 +0200

    added js variable update

--- src/script.js
+++ src/script.js
@@ -3,7 +3,8 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
-    console.log(suffix);
+    console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
